4fc3c6f1f3a7a33bebc4d3e135ae323d898ec0d5
Douglas Chimento <dchimento@gmail.com>
2024-03-05 11:01:11 +0200

    fmt

--- build.rs
+++ build.rs
@@ -2,12 +2,12 @@
 use std::process::Command;
 
 fn main() {
-    // Use git command or a Rust crate like git2 to get the commit hash
-    let output = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
-        .output()
-        .expect("Failed to execute git command");
+  // Use git command or a Rust crate like git2 to get the commit hash
+  let output = Command::new("git")
+    .args(["rev-parse", "HEAD"])
+    .output()
+    .expect("Failed to execute git command");
 
-    let git_hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
-    println!("cargo:rustc-env=GIT_COMMIT_HASH={}", git_hash);
+  let git_hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
+  println!("cargo:rustc-env=GIT_COMMIT_HASH={}", git_hash);
 }
