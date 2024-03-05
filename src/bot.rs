d4beee501a798589130bee98e89458ab19008a48
Douglas Chimento <dchimento@gmail.com>
2024-03-05 19:04:32 +0200

    remove teloxide::Bot

--- src/bot.rs
+++ src/bot.rs
@@ -10,7 +10,6 @@ use std::sync::Arc;
 use std::time::Instant;
 use teloxide::prelude::*;
 use teloxide::types::Recipient;
-use teloxide::Bot;
 use tracing::{debug, error, info};
 use warp::Filter;
 
