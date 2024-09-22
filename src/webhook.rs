a956cfad3143aca69bb790d926b52c4ca4df77ae
Douglas Chimento <dchimento@gmail.com>
2024-09-22 14:19:11 +0300

    helius sdk

--- src/webhook.rs
+++ src/webhook.rs
@@ -1,12 +1,12 @@
 use crate::command;
 use crate::command::WebhookCommands;
-use selene_helius_sdk::api::types::{AccountWebhookEncoding, TransactionType, TxnStatus};
-use selene_helius_sdk::api::webhook::{
-  CreateWebhookRequest, EditWebhookRequest, Webhook, WebhookData, WebhookType,
+use helius::types::{
+  AccountWebhookEncoding, Cluster, CreateWebhookRequest, EditWebhookRequest, TransactionStatus,
+  TransactionType, Webhook, WebhookType,
 };
-use selene_helius_sdk::{Cluster, Helius, HeliusBuilder};
+use helius::Helius;
 
-async fn list(helius: Helius) -> color_eyre::Result<()> {
+async fn list(helius: Helius) -> anyhow::Result<()> {
   let webhooks: Vec<Webhook> = helius.get_all_webhooks().await?;
   if webhooks.is_empty() {
     return Ok(println!("no webhooks found"));
@@ -19,42 +19,46 @@ async fn create(
   url: String,
   devnet: bool,
   addresses: Vec<String>,
-) -> color_eyre::Result<()> {
+) -> anyhow::Result<()> {
   let webhook_type = if devnet { WebhookType::EnhancedDevnet } else { WebhookType::Enhanced };
   let req = CreateWebhookRequest {
-    data: WebhookData {
-      webhook_url: url,
-      transaction_types: TransactionType::all(),
-      account_addresses: addresses,
-      webhook_type,
-      auth_header: None,
-      txn_status: TxnStatus::All,
-      encoding: AccountWebhookEncoding::JsonParsed,
-    },
+    webhook_url: url,
+    transaction_types: vec![TransactionType::Transfer],
+    account_addresses: addresses,
+    webhook_type,
+    auth_header: None,
+    txn_status: TransactionStatus::All,
+    encoding: AccountWebhookEncoding::JsonParsed,
   };
-  let response = helius.create_webhook(&req).await?;
+  let response = helius.create_webhook(req).await?;
   Ok(println!("id {}", response.webhook_id))
 }
 
-async fn delete(helius: Helius, id: String) -> color_eyre::Result<()> {
+async fn delete(helius: Helius, id: String) -> anyhow::Result<()> {
   helius.delete_webhook(&id).await?;
   Ok(())
 }
 
-async fn add(helius: Helius, id: String, mut addresses: Vec<String>) -> color_eyre::Result<()> {
+async fn add(helius: Helius, id: String, mut addresses: Vec<String>) -> anyhow::Result<()> {
   let mut hook = helius.get_webhook_by_id(&id).await?;
-  hook.webhook_data.account_addresses.append(&mut addresses);
+  hook.account_addresses.append(&mut addresses);
   helius
-    .edit_webhook(&EditWebhookRequest {
+    .edit_webhook(EditWebhookRequest {
       webhook_id: hook.webhook_id.clone(),
-      data: hook.webhook_data,
+      webhook_url: hook.webhook_url,
+      transaction_types: hook.transaction_types,
+      account_addresses: addresses,
+      webhook_type: hook.webhook_type,
+      auth_header: None,
+      txn_status: hook.txn_status,
+      encoding: hook.encoding,
     })
     .await?;
   Ok(())
 }
 
-pub(crate) async fn process_webhook(args: command::WebhookArgs) -> color_eyre::Result<()> {
-  let helius = HeliusBuilder::new(&args.helius_api_key).cluster(Cluster::MainnetBeta).build()?;
+pub(crate) async fn process_webhook(args: command::WebhookArgs) -> anyhow::Result<()> {
+  let helius = Helius::new_with_async_solana(&args.helius_api_key, Cluster::MainnetBeta)?;
   match args.command {
     None => list(helius).await,
     Some(WebhookCommands::List) => list(helius).await,
